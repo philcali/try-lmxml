ff6d48a9a82985e8ce62b1d6dc0861ac7398d4de
Philip Cali <philip.cali@gmail.com>
2011-11-20 23:26:58 -0600

    Using latest library

--- src/main/scala/LmxmlPlan.scala
+++ src/main/scala/LmxmlPlan.scala
@@ -12,26 +12,28 @@ import xml.PrettyPrinter
 
 case class Template(req: HttpRequest[HttpServletRequest]) {
   def apply(name: String) = {
-    val base = req.underlying.getRealPath(name + ".lmxml")
-    open(base).getLines.mkString("\n")
+    req.underlying.getRealPath(name + ".lmxml")
   }
 }
 
 object LmxmlText extends 
   Params.Extract("lmxml-input", Params.first ~> Params.nonempty)
 
-object Printer extends PrettyPrinter(150, 2)
+object XmlFormat extends PrettyPrinter(150, 2) with Function1[xml.NodeSeq, String] {
+  def apply(nodes: xml.NodeSeq) = formatNodes(nodes)
+}
 
 /** unfiltered plan */
 class LmxmlPlan extends unfiltered.filter.Planify({
   case req @ GET(Path("/")) =>
-    val template = Template(req)
-    val converted = Lmxml.convert(template("index"))(XmlConverter)
+    val converted = Lmxml.fromFile(Template(req)("index"))(XmlConvert)
+
     ContentType("text/html") ~>
-    ResponseString("<!DOCTYPE html>\n" + converted)
+    ResponseString(converted.toString)
   case POST(Path("/") & Params(LmxmlText(text))) =>
-    val resp = Lmxml(text).safeParseNodes(text).fold(_.toString, { parsed =>
-      Printer.formatNodes(XmlConverter.convert(parsed))
-    })
+    val process = XmlConvert andThen XmlFormat
+
+    val resp = Lmxml(text).safeParseNodes(text).fold(_.toString, process)
+
     ContentType("text/plain") ~> ResponseString(resp)
 });
